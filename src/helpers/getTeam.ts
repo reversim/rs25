ad4aa22311c3e4cb9f1bb6f1a4c5a265b85036cf
Inbar Danieli <inbar0308@gmail.com>
2024-12-18 21:08:27 +0200

    init

---
+++ src/helpers/getTeam.ts
@@ -0,0 +1,25 @@
+import slug from "slug";
+import teamData from "./teamData.json";
+
+export interface teamMember {
+  slug: string;
+  name: string;
+  title: string;
+  company: string;
+  bio: string;
+  linkedin: string;
+  twitter: string;
+  image: Promise<any>;
+}
+
+export async function getTeamMembers() {
+  return teamData.map((member) => {
+    const memberSlug = slug(member.name);
+
+    return {
+      ...member,
+      slug: memberSlug,
+      image: import(`../assets/team/${memberSlug}.png`),
+    };
+  });
+}
