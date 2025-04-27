ca80aef0ff17741dde26542f0ef5a490e6cca50b
Inbar Danieli <inbar0308@gmail.com>
2025-04-27 21:27:21 +0300

    improve font performance

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,24 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
   site: "https://summit2025.reversim.com",
+
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: "Montserrat",
+        cssVariable: "--font-montserrat",
+        weights: ["100..900"],
+        styles: ["normal"],
+        display: "swap",
+      },
+    ],
+  },
   integrations: [
     partytown({
       config: {
